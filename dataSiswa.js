8cc13ddaf4696c74d80f9bf063af273d2e654e15
nisfeb100 <milahana228@gmail.com>
2025-05-23 16:36:15 +0700

    Add files via upload

---
+++ dataSiswa.js
@@ -0,0 +1,622 @@
+const dataSiswa = {
+  "2518330001": {
+    "nama": "ADE LIVIA QAUDRUNADA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330002": {
+    "nama": "AFIQA BILQIS HUFAIDA ARBELLY",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330003": {
+    "nama": "AHMAD ALIMRAN MARASABESSY",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330004": {
+    "nama": "ALIF PUTRA MUBAROK",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330005": {
+    "nama": "AMELIA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330006": {
+    "nama": "BIMA FADEYWAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330007": {
+    "nama": "DEANDRA QIANA NURHADI PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330008": {
+    "nama": "FARHAN LUTFIR RAHMAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330009": {
+    "nama": "FITRIAH RAHMADANI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330010": {
+    "nama": "GARDA MAULANA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330011": {
+    "nama": "HERNI RISTIANA DEWI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330012": {
+    "nama": "KARTIKA AUDYA SARI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330013": {
+    "nama": "KEVIN FAUZI RIESFIANSA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330014": {
+    "nama": "LUTFIA NUR ANGGRAENI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330015": {
+    "nama": "MOAMMAR RIDWAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330016": {
+    "nama": "MUHAMAD HAIKAL HANNAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330017": {
+    "nama": "MUHAMMAD EIRDAN APPRILIO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330018": {
+    "nama": "MUHAMMAD RIZKY FADILLAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330019": {
+    "nama": "MUHAMMAD SYAHRIL HERMANSYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330020": {
+    "nama": "MUTHIA PUTRI RAMADHANY",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330021": {
+    "nama": "NABILA RAHMI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330022": {
+    "nama": "NAYLA ERKIYANSYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330023": {
+    "nama": "PRAVEEN NATHAN WARDHANA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330024": {
+    "nama": "RAHAYU DWI PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330025": {
+    "nama": "RAMA ELFANDI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330026": {
+    "nama": "REINA KEYSHA AMARAL",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330027": {
+    "nama": "RIZQY ARYA PRADIPTA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330028": {
+    "nama": "SAKINAH WULAN SAFITRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330029": {
+    "nama": "SHEILA AULIA PRASANTI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330030": {
+    "nama": "TASYA NOVA RIANINGTYAS",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330031": {
+    "nama": "TAUFIK IZA SAPUTRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330032": {
+    "nama": "ZAHIRA OKTAVIA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330033": {
+    "nama": "ADELE INARA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330034": {
+    "nama": "AMIRA ZAHRA FAUZIYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330035": {
+    "nama": "ANINDIRRA NUR ZAHIDAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330036": {
+    "nama": "ARYA ARMANSYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330037": {
+    "nama": "DEL NOEL RIDWAN NAYOAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330038": {
+    "nama": "DEVIANA AMIRA SALSABILA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330039": {
+    "nama": "FATHI IRFAN KAMIL",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330040": {
+    "nama": "GERRARD ALEXANDER TAMBUNAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330041": {
+    "nama": "GLORIA SEKARTAJI MUMPUNI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330042": {
+    "nama": "IRSYA ASSYIFA AULIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330043": {
+    "nama": "KEISANDRA NABILA HASAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330044": {
+    "nama": "KHALEY WALIMATUL BAHRAIN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330045": {
+    "nama": "MALIKA INARA AYUNINDYA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330046": {
+    "nama": "MEYSHA AFRIANI DEWI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330047": {
+    "nama": "MOCHAMMAD TABAH SEPTIO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330048": {
+    "nama": "MUHAMAD RIZKI ALPIANTO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330049": {
+    "nama": "MUHAMMAD FADHIL ALFAZRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330050": {
+    "nama": "MUHAMMAD RAFFI RAMADHAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330051": {
+    "nama": "MUHAMMAD SYAFIQURRAHMAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330052": {
+    "nama": "MUHAMMAD WAIS ALQORNI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330053": {
+    "nama": "NADILA DWI NOVIYANI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330054": {
+    "nama": "NAYLA SARDI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330055": {
+    "nama": "RADITYA KURNIAWAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330056": {
+    "nama": "RAIHANUM RAMADHANI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330057": {
+    "nama": "RAISYA MAULIDIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330058": {
+    "nama": "REYVANO RIZKI FEBRIANSYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330059": {
+    "nama": "SANDRIA PUTRI KRISDIANTO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330060": {
+    "nama": "TSANI NURAINI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330061": {
+    "nama": "WAHYU DWI PUTRA OKTAVIAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330062": {
+    "nama": "ZAHRA SALSABILA KURNIAWAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330063": {
+    "nama": "AFIKA APRILIANI GUSAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330064": {
+    "nama": "AFIQA SHAHIBAH ULYA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330065": {
+    "nama": "ALIKA NAILA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330066": {
+    "nama": "BEN IONNES VINCEN SIDABUTAR",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330067": {
+    "nama": "CAHYA KIRANA ATHIA RAZAQ",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330068": {
+    "nama": "DWIKA MAIRA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330069": {
+    "nama": "FADLY KURNIAWAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330070": {
+    "nama": "HAFIDZA AMANDA ZAHIRRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330071": {
+    "nama": "HISYAM RAMADHAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330072": {
+    "nama": "INTAN QURROTA A'YUNIN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330073": {
+    "nama": "JIHAN MYIESHA NAFEEZA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330074": {
+    "nama": "LARISA AZWA FAHIRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330075": {
+    "nama": "MASNAYA RACHMAT SAPUTRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330076": {
+    "nama": "MICHELLA NADINE AULIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330077": {
+    "nama": "MOHAMMAD FIDHAL ADRYAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330078": {
+    "nama": "MUHAMMAD FAHRI AKBAR",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330079": {
+    "nama": "MUHAMMAD RAFKA ARIANSYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330080": {
+    "nama": "MUTHI FAKHRUN NISA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330081": {
+    "nama": "NABILA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330082": {
+    "nama": "NAUFAL YUSUF FEBRIAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330083": {
+    "nama": "NAURA AZIZA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330084": {
+    "nama": "NEZYA ANDINA SARI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330085": {
+    "nama": "RAIHAN PRAYOGA HARYANTO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330086": {
+    "nama": "RAIISHA AQILLAH PUTRI APRIYANTO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330087": {
+    "nama": "RIFSKA AMALIA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330088": {
+    "nama": "SAMUDRA AL RIZQI PUDO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330089": {
+    "nama": "SEKAR AYU KINANTI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330090": {
+    "nama": "SYAKILA ANDITA PUTRI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330091": {
+    "nama": "TANIA ALMIRA RAMDHANI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330092": {
+    "nama": "VALERY AZZA TRIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330093": {
+    "nama": "ZIHAVIKA HELAN SATIVA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330094": {
+    "nama": "ADZKIA KHAIRA WIBOWO",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330095": {
+    "nama": "AFIKA PUTRI UTAMI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330096": {
+    "nama": "ALYA ANANDEA DESERA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330097": {
+    "nama": "BHADRIKA PRAYATA PUTRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330098": {
+    "nama": "CARISAWATI NISA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330099": {
+    "nama": "EGA PUTRA RAMADHAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330100": {
+    "nama": "FAIZ MAULANA HAKIM HASIBUAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330101": {
+    "nama": "FAIZAH NUR RAHMADHANI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330102": {
+    "nama": "FAJAR HIDAYANTO ARSYAD",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330103": {
+    "nama": "FARHANNA NUR ALAWIYAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330104": {
+    "nama": "FATHAN CHABIBIL MUJAB",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330105": {
+    "nama": "HANUNA RAMADHANIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330106": {
+    "nama": "IZZAN MALIKI PARYOKO PUTRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330107": {
+    "nama": "JUJU SUMIATI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330108": {
+    "nama": "KENZIE NARARYA RADHIKA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330109": {
+    "nama": "MUFARRIHIN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330110": {
+    "nama": "MUHAMMAD AL FATH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330111": {
+    "nama": "MUHAMMAD RAFA ABINAYA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330112": {
+    "nama": "NABILA DEWI NOVIYANA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330113": {
+    "nama": "NAUROTU ROHMAH AULIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330114": {
+    "nama": "QAF ALYA ZIKRILLAH",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330115": {
+    "nama": "RAMA AJI MURTI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330116": {
+    "nama": "RISKA APRILIA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330117": {
+    "nama": "RIZQI MUHAMMAD ICHSAN",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330118": {
+    "nama": "SANDRO EDWARD YEHEZKIEL HUTABARAT",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330119": {
+    "nama": "SATRIO ABIMANYU",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330120": {
+    "nama": "SHAFIRA AISA RIZKY",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330121": {
+    "nama": "SYAKIRA AZAHRA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330122": {
+    "nama": "SYIFA FEBRIYANTI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330123": {
+    "nama": "WULAN WIDIA LESTARI",
+    "status": "LULUS",
+    "password": "Munjul01"
+  },
+  "2518330124": {
+    "nama": "YUMNA FARIHA KAMILA",
+    "status": "LULUS",
+    "password": "Munjul01"
+  }
+};
